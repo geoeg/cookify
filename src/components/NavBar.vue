e8f9d81051b488872ad138c3cb1b01ecb0abe490
Georgi Kolev <georgi.lyubomirov.kolev@gmail.com>
2020-04-08 17:40:09 +0300

    setup home basic structure

---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app-bar app>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+    </div>
+    <v-toolbar-title>Cookify</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn v-for="item in items" :key="item.title" :to="item.link">
+        <v-icon left>{{ item.icon }}</v-icon>
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    items() {
+      let menuItems = [
+        {
+          title: "Recipes",
+          icon: "mdi-chef-hat",
+          link: "/recipes",
+        },
+        {
+          title: "Contact",
+          icon: "mdi-account-box",
+          link: "/contact",
+        },
+      ];
+      return menuItems;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,69 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
+
+    <v-row justify="center">
+      <p class="display-3 text-center ma-0 pt-4 pb-4 pt-sm-12 pb-sm-12">
+        This is a home page
+      </p>
+    </v-row>
+
+    <v-container fluid class="pt-0 pb-0">
+      <v-row align="center">
+        <v-col cols="12" sm="4">
+          <v-img src="../assets/jokata.jpg" alt="profile-pic"></v-img>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <p class="ma-0 headline">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            repudiandae, maxime velit, illo nam consectetur suscipit maiores
+            amet excepturi perspiciatis voluptatum voluptas eum fuga sunt
+            quisquam. A provident mollitia voluptates?
+          </p>
+        </v-col>
+      </v-row>
+
+      <v-divider class="mt-10 mb-10" />
+
+
+      <v-row align="center">
+        <v-col cols="12" sm="4">
+          <v-img src="../assets/jokata.jpg" alt="profile-pic"></v-img>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <p class="ma-0 body-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            repudiandae, maxime velit, illo nam consectetur suscipit maiores
+            amet excepturi perspiciatis voluptatum voluptas eum fuga sunt
+            quisquam. A provident mollitia voluptates?
+          </p>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-img src="../assets/jokata.jpg" alt="profile-pic"></v-img>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <p class="ma-0 body-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            repudiandae, maxime velit, illo nam consectetur suscipit maiores
+            amet excepturi perspiciatis voluptatum voluptas eum fuga sunt
+            quisquam. A provident mollitia voluptates?
+          </p>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-img src="../assets/jokata.jpg" alt="profile-pic"></v-img>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <p class="ma-0 body-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            repudiandae, maxime velit, illo nam consectetur suscipit maiores
+            amet excepturi perspiciatis voluptatum voluptas eum fuga sunt
+            quisquam. A provident mollitia voluptates?
+          </p>
+        </v-col>
+      </v-row>
+
+      <v-divider class="mt-10 mb-10" />
+
+    </v-container>
   </div>
 </template>
 
@@ -8,9 +71,12 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
-  components: {
-
-}
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      
+    }
+  }
+};
 </script>
